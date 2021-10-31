7e84397ce5fd90b670c8c43d121ef522a2be8497
Genie <genie0320@gmail.com>
2021-10-31 21:49:46 +0900

    pending text try

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,15 +2,20 @@ import { useEffect, useState } from "react"
 
 export const useFetch = (url) => {
     const [data, setData] = useState(null)
+    const [isPending, setIsPending] = useState(false)
 
     useEffect(() => {
+        setIsPending(true)
         const fetchData = async () => {
+
             const res = await fetch(url)
             const json = await res.json()
+
             setData(json)
         }
         fetchData()
+        setIsPending(false)
     }, [url])
 
-    return { data }
+    return { data, isPending }
 }
