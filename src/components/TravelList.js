f4def9658b8844fb9473fa512734e0f9dabc8a92
Genie <genie0320@gmail.com>
2021-10-31 21:16:25 +0900

    clean up

--- src/components/TravelList.js
+++ src/components/TravelList.js
@@ -8,8 +8,6 @@ import './TravelList.css'
 export default function TravelList() {
     const [url, setUrl] = useState('http://localhost:3000/travel')
     const { data: travels } = useFetch(url)
-    // 넘어오는 것은 data 인데, 이쪽 내부에서 사용할 이름은 travels이므로 변경.
-    // 만약, data 라는 이름으로 그대로 쓸 예정이라면 아래 travels.map => data.map으로 변경.
 
     return (
         <div className="travel-list">
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,38 +1,16 @@
 import { useEffect, useState } from "react"
 
-// hook의 이름은 반드시 use~로 시작해야 한다.
-// 아래와 같이 내보면, import { object } from 'module' 이렇게 받으면 된다.
 export const useFetch = (url) => {
     const [data, setData] = useState(null)
 
     useEffect(() => {
-        const fetchTravels = async () => {
+        const fetchData = async () => {
             const res = await fetch(url)
             const json = await res.json()
             setData(json)
         }
-        fetchTravels()
+        fetchData()
     }, [url])
 
-    // return { data : data } 이렇게 반환해도 되지만...동일한 이름이라면..
     return { data }
 }
-
-// export default useFetch
-// 위와 같이 내보내면 import 시 import moduleName from 'module' 로 받아야 한다.
-
-// const [travels, setTravels] = useState([])
-// const [url, setUrl] = useState('http://localhost:3000/travel')
-
-// const fetchTravels = useCallback(
-//     async () => {
-//         const response = await fetch(url)
-//         const json = await response.json()
-//         setTravels(json)
-//     }, [url])
-
-// useEffect(() => {
-//     fetchTravels()
-// }, [fetchTravels])
-
-// console.log(travels)
